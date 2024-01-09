b91d57303be669b1e19f71ff1e725f3c24d3fbba
hujuu <1961946+hujuu@users.noreply.github.com>
2024-01-09 21:50:51 +0900

    Update hardhat.config.cjs

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -3,4 +3,12 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  paths: {                         // add this 
+    artifacts: './src/artifacts',  // this is where our compiled contracts will go
+  },
+  networks: {                      // and this ... 
+    hardhat: {
+      chainId: 1337                // this is needed for MetaMask
+    }
+  }
 };
