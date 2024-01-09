31a2c35e9bfc8997dc46b41c46df40ca6eae1fba
hujuu <1961946+hujuu@users.noreply.github.com>
2024-01-09 23:46:45 +0900

    ethers@6

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { useState } from 'react';
-import { ethers } from 'ethers'; // ethers をインポートします
+import { ethers } from 'ethers';
 import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
 
 const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
@@ -14,7 +14,7 @@ function App() {
 
   async function fetchGreeting() {
     if (typeof window.ethereum !== 'undefined') {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const provider = new ethers.BrowserProvider(window.ethereum);
       const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider);
       try {
         const data = await contract.greet();
@@ -25,7 +25,7 @@ function App() {
     }    
   }
 
-  async function setGreeting() {
+  async function putGreeting() {
     if (!greeting) {
       console.log("Greeting is empty.");
       return;
@@ -33,8 +33,8 @@ function App() {
     if (typeof window.ethereum !== 'undefined') {
       try {
         await requestAccount();
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const signer = provider.getSigner();
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        const signer = await provider.getSigner();
         const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
         const transaction = await contract.setGreeting(greeting);
         await transaction.wait();
@@ -52,7 +52,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <button onClick={fetchGreeting}>Fetch Greeting</button>
-        <button onClick={setGreeting}>Set Greeting</button>
+        <button onClick={putGreeting}>Set Greeting</button>
         <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" />
       </header>
     </div>
