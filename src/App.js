d08cf9da1bc76572effbb3ae7ffeeb1f3a510a7b
hujuu <1961946+hujuu@users.noreply.github.com>
2024-01-15 00:20:08 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { useState } from 'react';
 import { ethers } from 'ethers';
 import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
@@ -7,7 +6,7 @@ import Token from './artifacts/contracts/Token.sol/Token.json';
 const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 const tokenAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 
-function App() {
+export default function App() {
   const [greeting, setGreetingValue] = useState();
   const [userAccount, setUserAccount] = useState();
   const [amount, setAmount] = useState();
@@ -89,5 +88,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
